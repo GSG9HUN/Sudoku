d11d1194923f0f4b51885b506e309ce592b67508
GSG9HUN <43461831+GSG9HUN@users.noreply.github.com>
2021-04-21 16:00:03 +0200

    Updated Sudoku tábla
    Sudoku tábla nézet impltementálva
    Másodlagos ablak kezdetleges implementáció.

--- src/sample/controller/GameController.java
+++ src/sample/controller/GameController.java
@@ -1,18 +1,46 @@
 package sample.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import sample.model.Game;
+import sample.model.StageChanger;
+
+import java.io.IOException;
+
 
 public class GameController {
 
     @FXML
-    GridPane GameTable;
-    Game game;
+    Label   L11,L12,L13,L14,L15,L16,L17,L18,L19,
+            L21,L22,L23,L24,L25,L26,L27,L28,L29,
+            L31,L32,L33,L34,L35,L36,L37,L38,L39,
+            L41,L42,L43,L44,L45,L46,L47,L48,L49,
+            L51,L52,L53,L54,L55,L56,L57,L58,L59,
+            L61,L62,L63,L64,L65,L66,L67,L68,L69,
+            L71,L72,L73,L74,L75,L76,L77,L78,L79,
+            L81,L82,L83,L84,L85,L86,L87,L88,L89,
+            L91,L92,L93,L94,L95,L96,L97,L98,L99;
 
 
-    public void onlyDigit(KeyEvent keyEvent) {
+    public void clickedLabel(MouseEvent mouseEvent) throws IOException {
 
+        System.out.println(mouseEvent.getTarget());
+        L11.setMouseTransparent(false);
+        L11.setDisable(true);
+        StageChanger.openNumbers("view/Numbers.fxml","Numbers");
+
+    }
+
+    public void solveYourself(MouseEvent mouseEvent) {
+    }
+
+    public void ready(MouseEvent mouseEvent) {
+    }
+
+    public void selectedDigit(MouseEvent mouseEvent) {
+        StageChanger.closeNumbers();
     }
 }
--- src/sample/model/StageChanger.java
+++ src/sample/model/StageChanger.java
@@ -10,13 +10,20 @@ import java.io.IOException;
 
 public class StageChanger {
     private static Stage primaryStage;
+    private static final Stage secondStage = new Stage();
+    private static boolean loaded = false;
 
 
     public StageChanger(Stage primaryStage){
         StageChanger.primaryStage = primaryStage;
     }
 
-    public static void loadStage( String path,@Nullable String title) throws IOException {
+
+    public static void loadStage(String path) throws IOException {
+        loadStage(path,null);
+    }
+
+    public static void loadStage( String path, String title) throws IOException {
         Parent root = FXMLLoader.load(StageChanger.class.getResource("../"+path));
 
         if(title!=null){
@@ -25,5 +32,26 @@ public class StageChanger {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    public static void openNumbers(String path,String title) throws IOException {
+
+        if(!loaded){
+            Parent root = FXMLLoader.load(StageChanger.class.getResource("../"+path));
+
+            if(title!=null){
+                secondStage.setTitle(title);
+            }
+            secondStage.setResizable(false);
+            secondStage.setScene(new Scene(root));
+            loaded=true;
+        }
+        secondStage.show();
+
+    }
+
+
+    public static void closeNumbers(){
+        secondStage.close();
     }
 }
