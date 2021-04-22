6d8d068095649187772c55043af6db8be477006d
GSG9HUN <43461831+GSG9HUN@users.noreply.github.com>
2021-04-22 12:33:12 +0200

    Fix and Feat
    Table labelelek javítása és a Sudoku tábla generálása.

--- src/sample/controller/GameController.java
+++ src/sample/controller/GameController.java
@@ -2,45 +2,66 @@ package sample.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
 import sample.model.Game;
 import sample.model.StageChanger;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 
 public class GameController {
 
+
+
+    public Label timer;
+
+    private Game game;
     @FXML
-    Label   L11,L12,L13,L14,L15,L16,L17,L18,L19,
-            L21,L22,L23,L24,L25,L26,L27,L28,L29,
-            L31,L32,L33,L34,L35,L36,L37,L38,L39,
-            L41,L42,L43,L44,L45,L46,L47,L48,L49,
-            L51,L52,L53,L54,L55,L56,L57,L58,L59,
-            L61,L62,L63,L64,L65,L66,L67,L68,L69,
-            L71,L72,L73,L74,L75,L76,L77,L78,L79,
-            L81,L82,L83,L84,L85,L86,L87,L88,L89,
-            L91,L92,L93,L94,L95,L96,L97,L98,L99;
+    public ArrayList<ArrayList<Label>> labels;
+
+
+
+
+    @FXML
+    public  void initialize() {
+        game = new Game();
+        setGui();
+    }
+
+    private  void setGui() {
+
+    labels.get(0).get(0).setDisable(true);
+
+        for (int i=0;i<9;i++){
+            for (int j=0;j<9;j++){
+                int num=game.getTableElement(i,j);
+                if(num!=0){
+                    labels.get(i).get(j).setText(Integer.toString(num));
+                    labels.get(i).get(j).setDisable(true);
+                }
+            }
+        }
+    }
 
 
     public void clickedLabel(MouseEvent mouseEvent) throws IOException {
 
-        System.out.println(mouseEvent.getTarget());
-        L11.setMouseTransparent(false);
-        L11.setDisable(true);
         StageChanger.openNumbers("view/Numbers.fxml","Numbers");
 
     }
 
     public void solveYourself(MouseEvent mouseEvent) {
+
+
     }
 
     public void ready(MouseEvent mouseEvent) {
     }
 
     public void selectedDigit(MouseEvent mouseEvent) {
+
         StageChanger.closeNumbers();
     }
 }
--- src/sample/model/Game.java
+++ src/sample/model/Game.java
@@ -1,7 +1,89 @@
 package sample.model;
 
-import sample.controller.OptionsController;
+import java.util.Random;
 
 public class Game {
+
+    int[][] table = new int[9][9];
+    int clues;
+    public Game(){
+       init();
+       createBoard();
+       solve();
+    }
+
+
+    private void createBoard() {
+        int cluesOnBoard=0;
+        Random rand = new Random();
+
+        while (cluesOnBoard!=clues){
+            int row = rand.nextInt(9);
+            int col = rand.nextInt(9);
+            while (table[row][col]==0){
+                int num = rand.nextInt(9)+1;
+                if(isSafe(row,col,num)){
+                    table[row][col]=num;
+                    cluesOnBoard++;
+                }
+            }
+
+        }
+
+    }
+
+    private void init() {
+
+        for(int i = 0; i<9; i++){
+            for (int j=0; j<9;j++) {
+                table[i][j]=0;
+            }
+        }
+
+        switch (Options.getDifficulties()){
+            case "Beginner": clues=30;
+                break;
+            case "Easy":clues=25;
+                break;
+            case "Medium": clues=20;
+                break;
+            case "Hard":clues=17;
+                break;
+        }
+    }
+
+    private void solve() {
+    }
+
+    private boolean isSafe(int row, int col, int num) {
+
+        for(int i=0;i<9;i++){
+            if(table[row][i]==num){
+                return false;
+            }
+        }
+
+        for(int i=0;i<9;i++){
+            if(table[i][col]==num){
+                return false;
+            }
+        }
+
+        int startRow =row-row%3 ;
+        int startCol=col-col%3;
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            if(table[i+startRow][j+startCol]==num) {
+                return false;
+            }
+        }
+    }
+
+        return true;
+    }
+
+    public int getTableElement(int row, int col) {
+        return table[row][col];
+    }
 
 }
